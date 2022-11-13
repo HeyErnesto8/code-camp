904d625ec148ab06c766106f7800071ccf3a6972
Ernesto <ernesto.trujillo@gmail.com>
2022-11-13 11:39:03 -0500

    First commit to check that's working

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # odin-recipes
-Project for code camp
+Project for code camp where is going to show assigment 1 as a link and some recipes
---
+++ greetingAssigment.js
@@ -0,0 +1,9 @@
+//Performing a task 
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName + '!');
+}
+
+let userName = prompt('Please, Whats your name?');
+let userLastName = prompt('And your last name?');
+
+greet('Ernesto', 'Trujillo');
